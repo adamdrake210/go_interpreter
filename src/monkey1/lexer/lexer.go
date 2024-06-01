a6fcc701e024359caf8410ae026cc0996dd0d6bb
Adam Drake <adam@chemify.io>
2024-06-01 09:36:10 +0200

    adding keywords lookup

--- src/monkey1/lexer/lexer.go
+++ src/monkey1/lexer/lexer.go
@@ -1,7 +1,6 @@
 package lexer
 
 import (
-	"fmt"
 	"monkey1/token"
 )
 
@@ -26,6 +25,19 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) readIdentifier() string {
+
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
@@ -55,8 +67,14 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
-	fmt.Printf("tok: %v\n", tok)
 
 	l.readChar()
 	return tok
